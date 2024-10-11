ed4c04506dcbd668defb01b778851b8b03e96e7d
Chikakoo <lionel.crafts@gmail.com>
2024-10-11 10:51:15 -0500

    Adjusted the script to work with JAR files; added out directory to gitignore

--- Adjust Pitch Bends and Vibrato.bat
+++ Adjust Pitch Bends and Vibrato.bat
@@ -3,6 +3,6 @@
 REM -p for pitch bend adjustments (default value assuming a range of 2)
 REM -v for vibrato adjustments (default range of 5)
 REM -v to clean up pitch bends to a tolerance of 16
-java MIDITools "%~1" -p -v -c pitch-bend 16
+java -jar MIDITools.jar "%~1" -p -v -c pitch-bend 16
 
 pause

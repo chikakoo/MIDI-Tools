7c1118a947b583d3fa709ec1e74ea0f560210a90
Chikakoo <lionel.crafts@gmail.com>
2024-10-11 13:34:05 -0500

    Documented the batch file with usage info and common event numbers

--- Adjust Pitch Bends and Vibrato.bat
+++ Adjust Pitch Bends and Vibrato.bat
@@ -1,5 +1,24 @@
 @echo off
 
+REM Run the command with no args for more detailed usage instructions
+
+REM Usage: usage: [midi filename] [--verbose (optional)] [a list of flags and their parameters]
+REM -p (pitch bend) [default range = 2]"
+REM -v (vibrato) [vibrato range = 5]
+REM -r (reverb) [reverb range = 26]
+REM -c (clean up) [event number] [tolerance = 10]
+REM -a (add) [event number] [amount]
+REM -s (subtract) [event number] [amount]
+
+REM --------------------------------------------------------------------
+REM For reference, commonly used event numbers for the -c, -a, and -s flags:
+REM - pitch-bend (for pitch bends - this is the only one that doesn't use a number)
+REM - 7 (Channel Volume)
+REM - 10 (Panpot - l/r panning)
+REM - 77 (VibratoDepth - vibrato in OoT)
+REM - 91 (ReverbSendLevel - reverb in OoT)
+
+REM --------------------------------------------------------------------
 REM -p for pitch bend adjustments (default value assuming a range of 2)
 REM -v for vibrato adjustments (default range of 5)
 REM -r for reverb adjustments (default range of 26)
